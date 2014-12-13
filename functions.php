a720f683d64728d12845de7e6764a84f7a55e82d
Aldo Gonzalez Carlos <aldogcarlos@hotmail.com>
2014-12-12 22:19:38 -0600

    Lightbox
    Lightbox plugin

--- functions.php
+++ functions.php
@@ -5,12 +5,14 @@ function watchdogs_scripts() {
 	
 	wp_enqueue_style( 'normalize.css', get_template_directory_uri() . '/css/normalize.css' );
 	wp_enqueue_style( 'bootstrap.css', get_template_directory_uri() . '/css/bootstrap/css/bootstrap.min.css' );
+	wp_enqueue_style( 'lightbox', get_template_directory_uri() . '/css/lightbox.css' );
 	wp_enqueue_style( 'style.css', get_bloginfo( 'stylesheet_url' ) );
 	
 	wp_enqueue_script( 'jquery' );
 	wp_enqueue_script( 'watchdogs', get_template_directory_uri() . '/js/watchdogs.js', array('jquery'), false, true );
 	wp_enqueue_script( 'bootstrap.js', get_template_directory_uri() . '/js/bootstrap/js/bootstrap.min.js', array('jquery'), false, true );
 	wp_enqueue_script( 'jquery.scrollTo.min.js', get_template_directory_uri() . '/js/jquery.scrollTo.min.js', array('jquery'), false, true );
+	wp_enqueue_script( 'lightbox.min.js', get_template_directory_uri() . '/js/lightbox.min.js', array('jquery'), false, true );
 }
 add_action( 'wp_enqueue_scripts', 'watchdogs_scripts' );
 add_theme_support( 'post-thumbnails' );
