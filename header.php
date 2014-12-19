cdaa90f60f0188f3f7f22b9d4ba9dccc04e511a0
Aldo Gonzalez Carlos <aldogcarlos@hotmail.com>
2014-12-18 21:42:58 -0600

    Email Changes
    Performed the most of changes on last emial with subjecct
    "Fwd: Re: Watch Dog Site Review​"
    
    Pending this one
    query_posts() should never be used directly.
    
    query_posts(array(
    'posts_per_page' => -1,
    'meta_key' => 'weight',
    'orderby' => 'meta_value',
    'order' => 'ASC'
    ));
    
    https://codex.wordpress.org/Function_Reference/query_posts (Note: This
    function isn't meant to be used by plugins or themes. )

--- header.php
+++ header.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
@@ -9,16 +10,14 @@
     	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 	    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 	    <!--[if lt IE 9]>
-	      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-	      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+	      <script src="<?php echo get_template_directory_uri(); ?>/js/html5shiv.js"></script>
+	      <script src="<?php echo get_template_directory_uri(); ?>/js/respond.min.js"></script>
 	    <![endif]-->							
-		
+		<script>var homeurl = "<?php bloginfo('home'); ?>";</script>
 		<?php wp_head(); ?>			
-		
-		<script>var homeurl = "<?php bloginfo('home'); ?>";</script>		
 	</head>
 
-	<body>	
+	<body <?php body_class( $class ); ?>>	
 		<div id="content-wrap">
 			<div class="top-nav">
 				<div class="container">
